e713c729beaca69ca0b2bda99e34a1c4d028b220
山川 <18487115313@163.com>
2023-03-24 10:52:00 +0800

    [添加] 添加templates

---
+++ template/tailwind.config.cjs
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
